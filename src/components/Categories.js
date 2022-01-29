9a01ef952e39a01765ac4fead19785fb5ad69789
Jorge Adalberto Rodriguez Garcia <george_nlc@hotmail.com>
2022-01-28 18:59:56 -0600

    Categories component version: 1 done

---
+++ src/components/Categories.js
@@ -0,0 +1,12 @@
+const Categories = () => {
+    return (
+        <>
+            <ul className="categories">
+                <li>Nike</li>
+                <li>Puma</li>
+                <li>Adidas</li>
+                <li>Vans</li>
+            </ul>
+        </>
+    );
+}
