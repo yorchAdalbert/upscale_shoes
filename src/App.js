33ce1a08159334d2fe304eceeeeaa10283eebd5a
Jorge Adalberto Rodriguez Garcia <george_nlc@hotmail.com>
2022-02-01 22:27:24 -0600

    Adding the items section and fixed the nav-bar style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/ItemListContainer";
 
 function App() {
   return (
       <div className="App">
         <NavBar />
+        <ItemListContainer
+          greeting='Este la sección de los items'
+        />
     </div>
   );
 }
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+const ItemListContainer = props => {
+    return (
+        <div className="items-container">
+            <h2>{props.greeting}</h2>
+        </div>
+    );
+}
+
+export default ItemListContainer;
