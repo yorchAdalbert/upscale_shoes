fbae44faaa4aa0e3de638c482346303848875513
Jorge Adalberto Rodriguez Garcia <george_nlc@hotmail.com>
2022-02-01 22:08:28 -0600

    Adding and styled the cartWidget component

---
+++ src/components/CartWidget.js
@@ -0,0 +1,10 @@
+const CartWidget = () => {
+    return (
+        <div className="cart-shoping-area">
+            <img src="./cart-shoping.png" alt="Cart shoping item" className="cart-shoping" />
+            <span className="item-counter">5</span>
+        </div>
+    )
+}
+
+export default CartWidget;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,13 @@
 import Brand from "./Brand";
 import Categories from "./Categories";
+import CartWidget from "./CartWidget";
 
 const NavBar = () => {
     return (
         <div className="nav-bar">
             <Brand />
             <Categories />
+            <CartWidget />
         </div>
     )
 }
